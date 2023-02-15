b6ba2700b05696722a24260b476bf30df85f3c4b
HieuPH <bm.hcmc.hieuph@modetour.com>
2023-02-15 10:15:54 +0700

    update

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -184,7 +184,7 @@ const forgotPasswordToken = asyncHandler(async (req, res) => {
   try {
     const token = await user.createPasswordResetToken();
     await user.save();
-    const resetURL = `Hi, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href="http://localhost:4000/api/user/reset-password/${token}">Click Here</a>`;
+    const resetURL = `Hello, Please follow this link to reset Your Password. This link is valid till 10 minutes from now. <a href="http://localhost:4000/api/user/reset-password/${token}">Click Here</a>`;
     const data = {
       to: email,
       text: "Hey User",
